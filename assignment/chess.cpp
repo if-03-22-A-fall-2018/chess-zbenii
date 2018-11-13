738159c2b07f3b0b7cbc16aad39917463eff8566
zbenii <besic.beni@gmx.at>
2018-11-13 11:15:49 +0100

    2 tests failed

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -12,14 +12,15 @@
  */
 
  #include <stdio.h>
-  #include "math.h"
+  #include <math.h>
   #include "general.h"
   #include "chess.h"
 
   bool 	is_square_ok (File file, Rank rank)
   {
-   return (file - 'a' >= 1 && file - 'a' <= 8 && rank >= 1 && rank <= 8 );
+   return (file >= 'a'&& file <= 'h' && rank >= 1 && rank <= 8 );
   }
+
   bool 	is_move_from_base_line (enum PieceColor color, Rank rank)
   {
     if (color == Black) {
@@ -31,10 +32,12 @@
 
     return false;
   }
+
   bool 	is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type)
   {
     return (pc.color == color && pc.type == type);
   }
+
   void 	init_chess_board (ChessBoard chess_board)
   {
     for(int i=0; i<8;i++){
@@ -44,32 +47,43 @@
       }
     }
   }
+
   struct ChessSquare * 	get_square (ChessBoard chess_board, File file, Rank rank)
   {
-    if (is_square_ok(file, rank)) {
-      struct ChessSquare x;
-      x.is_occupied=chess_board[file-'a'][rank-1].is_occupied;
-      x.piece=chess_board[file-'a'][rank-1].piece;
-      return &x;
+    if (is_square_ok(file, rank))
+    {
+
+      return &chess_board[rank-1][file-'a'];
     }
+    else{return 0;}
   }
+
   bool 	is_square_occupied (ChessBoard chess_board, File file, Rank rank)
   {
-   return (chess_board[file - 1][rank - 1].is_occupied);
+   return (chess_board[rank - 1][file- 'a'].is_occupied);
   }
+
   bool 	add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece)
    {
-     if (chess_board[file - 1][rank - 1].is_occupied == false && is_square_ok(file, rank) == true) {
-       chess_board[file-1][rank-1].is_occupied = true;
-       chess_board[file-1][rank-1].piece = piece;
+     if (chess_board[rank-1][file-'a'].is_occupied == false && is_square_ok(file, rank) == true) {
+       chess_board[rank-1][file-'a'].is_occupied = true;
+       chess_board[rank-1][file-'a'].piece = piece;
        return true;
      }
      return false;
    }
-  struct ChessPiece 	get_piece (ChessBoard chess_board, File file, Rank rank)
+
+  struct ChessPiece get_piece (ChessBoard chess_board, File file, Rank rank)
   {
-    return chess_board[file - 1][rank -1].piece;
+    struct ChessPiece x;
+    x.type=NoPiece;
+    if(is_square_ok(file,rank)==true)
+    {
+      return chess_board[rank- 1][file-'a'].piece;
+    }
+    else{return x;}
   }
+
   void 	setup_chess_board (ChessBoard chess_board)
   {
     init_chess_board(chess_board);
@@ -95,26 +109,41 @@
   	add_piece(chess_board, 'd', 8, {Black, Queen});
   	add_piece(chess_board, 'e', 8, {Black, King});
   }
+
   bool 	remove_piece (ChessBoard chess_board, File file, Rank rank)
   {
     if (is_square_occupied(chess_board,file,rank) == true) {
-      chess_board[file-1][rank-1] = {0};
+      chess_board[rank-1][file-'a'] = {0};
       return true;
     }
     return false;
   }
+
   bool 	squares_share_file (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
   {
-    return (s1_f == s2_f);
+    if(is_square_ok(s1_f,s1_r)==true&&is_square_ok(s2_f,s2_r)==true)
+    {
+      return (s1_f == s2_f);
+    }
+    else{return false;}
+
   }
+
   bool 	squares_share_rank (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
   {
-    return (s1_r == s2_r);
+    if(is_square_ok(s1_f,s1_r)==true&&is_square_ok(s2_f,s2_r)==true)
+    {
+      return (s1_r == s2_r);
+    }
+    else{return false;}
+
   }
+
   bool 	squares_share_diagonal (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
   {
     return fabs((double)s1_f-s2_f)==fabs((double)s1_r-s2_r)&&is_square_ok(s1_f,s1_r)&&is_square_ok(s2_f, s2_r);
   }
+
   bool 	squares_share_knights_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
   {
     if (is_square_ok(s1_f,s1_r) == true && is_square_ok(s2_f,s2_r) == true) {
@@ -122,13 +151,15 @@
     }
     return false;
   }
+
   bool 	squares_share_pawns_move (enum PieceColor color, enum MoveType move, File s1_f, Rank s1_r, File s2_f, Rank s2_r)
   {
-    if (s1_r+1 == s2_r && (s1_f-1 == s2_f || s1_f+1 == s2_f)) {
+    if (is_square_ok(s1_f,s1_r) == true && is_square_ok(s2_f,s2_r) == true && s1_r+1 == s2_r && (s1_f-1 == s2_f || s1_f+1 == s2_f)) {
       move = CaptureMove;
-    }else{move = NormalMove;}
-    return (move == CaptureMove);
+    }else {move = NormalMove;}
+    return move;
   }
+
   bool 	squares_share_queens_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
   {
     if (is_square_ok(s1_f,s1_r) == true && is_square_ok(s2_f,s2_r) == true)
@@ -137,6 +168,7 @@
     }
     return false;
   }
+
   bool 	squares_share_kings_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
   {
     if (is_square_ok(s1_f,s1_r) == true && is_square_ok(s2_f,s2_r) == true) {
